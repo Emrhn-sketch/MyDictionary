776439b04cc4b6850b6167c0eb30539a83dee445
TopalFare <TopalFare@DESKTOP-66L029J>
2021-01-22 23:29:42 +0300

    .Gitignore ve .gitattributes ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] _keys;
+        Value[] _values;
+        public MyDictionary()
+        {
+            _keys = new Key[0];
+            _values = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeys = _keys;
+            Value[] tempValues = _values;
+
+            _keys = new Key[_keys.Length + 1];
+            _values = new Value[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                tempKeys[i] = _keys[i];
+                tempValues[i] = _values[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericsIntro;
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "A book");
+            dictionary.Add(2, "B book");
+        }
+    }
+}
